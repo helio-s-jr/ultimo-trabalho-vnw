0a0c0f67700b4c158717ba252158a8452fe90c50
Helio Junior <heliojr.zpt@gmail.com>
2022-11-11 17:52:16 -0300

    Contatos iniciado

--- src/Pages/Contatos/index.jsx
+++ src/Pages/Contatos/index.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import * as S from './style'
 import Header from '../../Components/Header/index'
 import Footer from '../../Components/Footer/index'
 
 export default function Contatos(){
+    const { register, handleSubmit } = useForm();
+    const [data, setData] = useState('');
+
     return(
         <S.ContainerContacts>
             <Header/>
             <S.ContainerMainContacts>
-                <h1>Pagina para todos os contatos</h1>
+                <form>
+                    <input {...register('fullName')} placeholder='Nome Completo' />
+                    <textarea {...register('yourMensager')} placeholder='Digite aqui sua mensagem'/>
+                    <p>{data}</p>
+                    <input type='submit'/>
+                </form>
             </S.ContainerMainContacts>
             <Footer/>
         </S.ContainerContacts>
