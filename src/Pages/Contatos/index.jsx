2d11ebf6c9fa1937b3f021cfc75182025206832c
Helio Junior <heliojr.zpt@gmail.com>
2022-11-14 23:39:53 -0300

    Página de contatos atualizada

--- src/Pages/Contatos/index.jsx
+++ src/Pages/Contatos/index.jsx
@@ -1,23 +1,68 @@
 import React from 'react'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import emailjs from '@emailjs/browser'
 import * as S from './style'
 import Header from '../../Components/Header/index'
 import Footer from '../../Components/Footer/index'
+import { ThemeConsumer } from 'styled-components'
 
 export default function Contatos(){
-    const { register, handleSubmit } = useForm();
-    const [data, setData] = useState('');
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    function sendEmail(e){
+        e.preventDefault();
+
+        if(name === '' || email === '' || message === ''){
+            alert('Favor, preencher todos os campos');
+            return;
+        }
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+        emailjs.send('service_2obj23u', 'template_i64h2ym','templateParams', '7s9t5arp0PeGjrtiw')
+        .then((response)=>{
+            console.log('EMAIL ENVIADO', response.status, response.text)
+            setName(''),
+            setEmail(''),
+            setMessage('')
+        }, (err) => {
+            console.log('Erro: ', err)
+        })
+    }
 
     return(
         <S.ContainerContacts>
             <Header/>
             <S.ContainerMainContacts>
-                <form>
-                    <input {...register('fullName')} placeholder='Nome Completo' />
-                    <textarea {...register('yourMensager')} placeholder='Digite aqui sua mensagem'/>
-                    <p>{data}</p>
-                    <input type='submit'/>
+                <h1>Contato</h1>
+                <form onSubmit={sendEmail}>
+                    <input 
+                        type='text' 
+                        placeholder='Digite seu nome'
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                    <input 
+                        type='text' 
+                        placeholder='Digite seu email'
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                    />
+                    <textarea 
+                        type='text' 
+                        placeholder='Digite sua mensagem'
+                        onChange={(e) => setMessage(e.target.value)}
+                        value={message}
+                    />
+                    <input 
+                        type='submit' 
+                        value='Enviar'
+                    />
                 </form>
             </S.ContainerMainContacts>
             <Footer/>
